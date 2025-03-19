24e14893fa2245cbced8cfcf64ec57627333067e
Dennis Wachira <wachiradenis336@gmail.com>
2025-03-19 14:27:07 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+// Declare customerName in global scope
+var customerName = 'bob';
+
+// Function to uppercase customerName
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+// Function to set bestCustomer in global scope
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+// Function to overwrite bestCustomer
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+// Declare leastFavoriteCustomer as a constant in global scope
+const leastFavoriteCustomer = 'someone';
+
+// Function attempting to change leastFavoriteCustomer
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'another person'; 
+}
